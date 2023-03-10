16a4476df25e730b08e525a8313c41e53f55559e
MahekkumarChaniyara <mahekkumar.chaniyara@marutitech.com>
2023-03-10 17:39:57 +0530

    add

--- src/Component/AddContact.js
+++ src/Component/AddContact.js
@@ -20,65 +20,77 @@ const AddContact = (props) => {
 
   return (
     <>
-      {errorData && (
-        <>
-          <p>{errorData.fullnameerror}</p>
-          <p>{errorData.emailerror}</p>
-          <p>{errorData.phoneerror}</p>
-        </>
-      )}
       <Form method={props.method}>
         <div className={classes.mainform}>
+          <h4><span className={classes.span}>*</span> field is mendetory.</h4>
           <div className={classes.smallparent}>
-            {errorData && (
-              <>
-                <p>{errorData.fullnameerror}</p>
-              </>
-            )}
             <div className={classes.small}>
-              <label htmlFor="name" className={errorData&&errorData.fullnameerror&&classes.labelError}>Enter Full Name</label>
+              <label
+                htmlFor="name"
+                className={
+                  errorData && errorData.fullnameerror && classes.labelError
+                }
+              >
+                Enter Full Name<span className={classes.span}>*</span>
+              </label>
               <input
                 id="name"
                 name="name"
                 type="text"
                 placeholder="Person Full Name"
                 defaultValue={dataContact && dataContact.contact.fullname}
-                className={errorData&&errorData.fullnameerror&&classes.inputError}
+                className={
+                  errorData && errorData.fullnameerror && classes.inputError
+                }
+              ></input>
+            </div>
+            {errorData && (
+              <>
+                <p>{errorData.fullnameerror}</p>
+              </>
+            )}
+          </div>
+
+          <div className={classes.smallparent}>
+            <div className={classes.small}>
+              <label
+                htmlFor="email"
+                className={
+                  errorData && errorData.emailerror && classes.labelError
+                }
+              >
+                Email Id<span className={classes.span}>*</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="test@test.com"
+                defaultValue={dataContact && dataContact.contact.email}
+                className={
+                  errorData && errorData.emailerror && classes.inputError
+                }
+              ></input>
+            </div>
+            {errorData && (
+                <>
+                  <p>{errorData.emailerror}</p>
+                </>
+              )}
+          </div>
+
+          <div className={classes.smallparent}>
+            <div className={classes.small}>
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                defaultValue={dataContact && dataContact.contact.address}
               ></input>
             </div>
           </div>
 
-          <div className={classes.smallparent}>
-            {errorData && (
-              <>
-                <p>{errorData.emailerror}</p>
-              </>
-            )}
-            <div className={classes.small}>
-            <label htmlFor="email" className={errorData&&errorData.emailerror&&classes.labelError}>Email Id</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              placeholder="test@test.com"
-              defaultValue={dataContact && dataContact.contact.email}
-              className={errorData&&errorData.emailerror&&classes.inputError}
-            ></input>
-          </div>
-          </div>
-          
-          <div className={classes.smallparent}>
-            <div className={classes.small}>
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              name="address"
-              id="address"
-              defaultValue={dataContact && dataContact.contact.address}
-            ></input>
-          </div>
-          </div>
-          
           <div className={classes.smallparent}>
             <div className={classes.small}>
               <label htmlFor="company">Company Name</label>
@@ -90,25 +102,34 @@ const AddContact = (props) => {
               ></input>
             </div>
           </div>
-          
+
           <div className={classes.smallparent}>
-            {errorData && (
-              <>
-                <p>{errorData.phoneerror}</p>
-              </>
-            )}
             <div className={classes.small}>
-              <label htmlFor="number" className={errorData&&errorData.phoneerror&&classes.labelError}>Phone Number</label>
+              <label
+                htmlFor="number"
+                className={
+                  errorData && errorData.phoneerror && classes.labelError
+                }
+              >
+                Phone Number<span className={classes.span}>*</span>
+              </label>
               <input
                 type="number"
                 id="number"
                 name="number"
                 defaultValue={dataContact && dataContact.contact.phone}
-                className={errorData&&errorData.phoneerror&&classes.inputError}
+                className={
+                  errorData && errorData.phoneerror && classes.inputError
+                }
               ></input>
             </div>
+            {errorData && (
+              <>
+                <p>{errorData.phoneerror}</p>
+              </>
+            )}
           </div>
-          
+
           <div className={classes.buttons}>
             <button type="button" onClick={clickHandler}>
               Cancel
