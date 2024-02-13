2d0c9c21ba544901d67a6c1b8475d73d91189b8f
MahekkumarChaniyara <mahekkumar.chaniyara@marutitech.com>
2024-02-13 17:04:31 +0530

    notification changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import NewContact,{loader as loadContacts} from "./Component/NewContact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NewContact, { loader as loadContacts } from "./Component/NewContact";
 import NewPage from "./Component/NewPage";
 import Header from "./Component/header";
 import ErrorPage from "./Pages/ErrorPage";
@@ -12,26 +12,56 @@ import { loader as loaderContactDetails } from "./Component/ContactDetail";
 import EditContactPage from "./Pages/EditContactPage";
 
 function App() {
-  const route=createBrowserRouter([
-    {path:'/',element:<Header/>,errorElement:<ErrorPage/>,children:[
-      {index:true,element:<NewPage/>},
-      {
-        path:'contact',element:<NewContactPage/>,id:'loadcontact',loader:loadContacts,children:[
-          {path:'',element:<NewContact/>},
-          {path:'addcontact',element:<AddContactPage/>,children:[
-            {index:true,element:<AddContact method="POST"/>,action:saveContactAction},  
-          ]},
-          {path:':contactId',id:'individual',loader:loaderContactDetails,children:[
-            {index:true,element:<ContactDetail/>},
-            {path:'edit',element:<EditContactPage/>,children:[{index:true,element:<AddContact method="PATCH"/>,action:saveContactAction}]}
-          ]},     
-        ]      
-      }, 
-    ]},
-  ])
-  return (
-    <RouterProvider router={route}></RouterProvider>
-  );
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <Header />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <NewPage /> },
+        {
+          path: "contact",
+          element: <NewContactPage />,
+          id: "loadcontact",
+          loader: loadContacts,
+          children: [
+            { path: "", element: <NewContact /> },
+            {
+              path: "addcontact",
+              element: <AddContactPage />,
+              children: [
+                {
+                  index: true,
+                  element: <AddContact method="POST" />,
+                  action: saveContactAction,
+                },
+              ],
+            },
+            {
+              path: ":contactId",
+              id: "individual",
+              loader: loaderContactDetails,
+              children: [
+                { index: true, element: <ContactDetail /> },
+                {
+                  path: "edit",
+                  element: <EditContactPage />,
+                  children: [
+                    {
+                      index: true,
+                      element: <AddContact method="PATCH" />,
+                      action: saveContactAction,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={route}></RouterProvider>;
 }
 
 export default App;
--- src/Component/NewPage.js
+++ src/Component/NewPage.js
@@ -1,15 +1,31 @@
 import { useNavigation } from "react-router-dom";
-import classes from "./NewPage.module.css"
-import {IoMdContact} from "react-icons/io"
+import classes from "./NewPage.module.css";
+import { IoMdContact } from "react-icons/io";
+import { useEffect } from "react";
 
-const NewPage=()=>{
-    const nav=useNavigation();
-    return (<>
-        <h1 >
-           Hello, This is Home Page please go to Contact Page on click <IoMdContact style={{textAlign:'center'}}/>
-        </h1>
-        {nav.state==="loading" && <div className={classes.loading}>Loading...</div>}
-    </>)
-}
+const NewPage = () => {
+  const nav = useNavigation();
+  const notification = () => {
+    return "Notification" in window
+      ? Notification.requestPermission().then((res, err) => {
+          console.log(res, err);
+        })
+      : null;
+  };
+  useEffect(() => {
+    notification();
+  }, []);
+  return (
+    <>
+      <h1>
+        Hello, This is Home Page please go to Contact Page on click{" "}
+        <IoMdContact style={{ textAlign: "center" }} />
+      </h1>
+      {nav.state === "loading" && (
+        <div className={classes.loading}>Loading...</div>
+      )}
+    </>
+  );
+};
 
 export default NewPage;
